45f27f78671a0fc380a13da980482b8933acf4ac
pjt3591oo <pjt3591oo@gmail.com>
2024-09-17 10:53:40 +0900

    kakao 전처리 수정

--- ch3/kakaotalk_chat_sentiment_analysis/app.py
+++ ch3/kakaotalk_chat_sentiment_analysis/app.py
@@ -1,6 +1,5 @@
 from datasets import load_dataset
 from transformers import pipeline
-import json
 
 dataset = load_dataset('csv', data_files="kakaotalk_chat_sentiment_analysis/kakaochat.csv")
 model_id = 'hun3359/klue-bert-base-sentiment'
@@ -15,53 +14,41 @@ Message
     삭제된 메시지입니다.
     사진
     이모티콘
+    {USER}님이 나갔습니다.
+    {USER}님이 들어왔습니다.
 '''
 
 def filter_row(row):
-    if row['User'] == '방장봇':
+    user = row['User']
+    
+    if user == '방장봇':
         return None
-    if row['Message'] in ['삭제된 메시지입니다.', '사진', '이모티콘']:
+    if row['Message'] in ['삭제된 메시지입니다.', '사진', '이모티콘', '님이 나갔습니다.', '님이 들어왔습니다.']:
         return None
+    if row['Message'].startswith('http'):
+        return None
+    if f'{user}님이 나갔습니다.' == row['Message']:
+        return None
+    if f'{user}님이 들어왔습니다.' == row['Message']:
+        return None
+    
     return row
 
 def add_sentiment(row):
     try:
         rst = model_pipeline(row['Message'])
-        print(rst)
-        row['Sentiment'] = rst[0]['label']
-        row['Score'] = rst[0]['score']
+        row.setdefault('sentiment', rst[0].get('label', ''))
+        row.setdefault('rate', str(rst[0].get('score', 0)))
     except:
-        row['Sentiment'] = 'error'
+        row['sentiment'] = 'error'
+        row['rate'] = '0'
+
     return row
 
 print('before: ', dataset['train'])
 did_filter_dataset = dataset.filter(filter_row)
 did_filter_dataset['train'].to_csv('kakaotalk_chat_sentiment_analysis/kakaochat_filtered.csv')
 
-print('after: ', did_filter_dataset['train'])
-sentiment_dataset = did_filter_dataset.map(add_sentiment)
-sentiment_dataset['train'].to_csv('kakaotalk_chat_sentiment_analysis/kakaochat_sentiment.csv')
-
-
-# user_sentiment = {}
-# count = 1
-# dataset_size = len(dataset['train'])
-# for row in dataset['train']:
-#     try :
-#         rst = model_pipeline(row['Message'])
-#         print(f'============  {count} / {dataset_size}  ============')
-#         # print('user: ', row['User'])
-#         print('MSG : ', row['Message'])
-#         print('result: ', rst)
-#         user_sentiment.setdefault(row['User'], {})
-#         user_sentiment[row['User']].setdefault(rst[0]['label'], 0)
-#         user_sentiment[row['User']][rst[0]['label']] += 1
-#         count += 1
-#         print('========================')
-#         print('')
-#         print('')
-#     except:
-#         print('>>>>>> error: ', row['Message'])
-
-# with open('./user_sentiment.json','w') as f:
-#   json.dump(user_sentiment, f, ensure_ascii=False, indent=4)
+# print('after: ', did_filter_dataset['train'])
+# sentiment_dataset = did_filter_dataset.map(add_sentiment)
+# sentiment_dataset['train'].to_csv('kakaotalk_chat_sentiment_analysis/kakaochat_sentiment.csv')
